ef6e325d53460894820ac9b61624385d9a0247d8
Mihir1911 <42714713+Mihir1911@users.noreply.github.com>
2020-09-30 20:58:47 +0530

    after tipsy

---
+++ app/src/main/java/com/example/travelnew/Places.java
@@ -0,0 +1,14 @@
+package com.example.travelnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Places extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+    }
+}
